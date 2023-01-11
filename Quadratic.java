7f03d5ecf2d695b252c9ba71bc240aebec22fe7b
Atley St. Vrain <atleystvrain@gmail.com>
2023-01-11 11:33:25 -0600

    Complete Assignment

--- Quadratic.java
+++ Quadratic.java
@@ -1,5 +1,19 @@
 public class Quadratic {
     public static void main(String[] args) {
-        
-    }
-}
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+
+        System.out.println("a = " + a);
+        System.out.print("b = " + b);
+        System.out.print("c = " + c);
+
+        double result = (-b + Math.sqrt(b*b - 4 * a * c))/(2 * a);
+        double result2 = (-b - Math.sqrt(b*b - 4 * a * c))/(2 * a);
+        System.out.println(result);
+        System.out.println(result2);
+    }}
+
+
+
+      
