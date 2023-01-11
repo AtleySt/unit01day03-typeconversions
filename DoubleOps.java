0ca56c88f29ce2d28b3ef59aa133152c1048a85a
Atley St. Vrain <atleystvrain@gmail.com>
2023-01-11 11:37:02 -0600

    Complete Assignment

--- DoubleOps.java
+++ DoubleOps.java
@@ -1,5 +1,22 @@
 class DoubleOps {
     public static void main(String[] args) {
-        System.out.println("hello");
+        //System.out.println("Arg 1: " + args[0]);
+        //System.out.println("Arg 2: " + args[1]);
+
+        //abs command
+        System.out.println("The absolute value of " + args[0] + " is " +Math.abs(Integer.parseInt(args[0])));
+
+        //double abs
+        System.out.println("The absolute value of " + args[1] + " is " + Math.abs(Double.parseDouble(args[1])));
+
+        //double pow
+        System.out.println(args[0]+ " to the power of " + args[1] + " is " +Math.pow(Double.parseDouble(args[0]), Double.parseDouble(args[1])));
+
+        //double sqrt
+        System.out.print("The square root of " + args[0] + " is " +Math.sqrt(Double.parseDouble(args[0])));
+        System.out.println("The square root of " + args[1] + " is " +Math.sqrt(Double.parseDouble(args[1])));
+
+        //double random
+        System.out.println("Your random number is " + Math.random());
     }
 }
--- TrigIdentity.java
+++ TrigIdentity.java
@@ -1,5 +1,12 @@
 public class TrigIdentity {
     public static void main(String[] args) {
-        
+        System.out.println(Math.sin(Double.parseDouble(args[0])));
+        System.out.println(Math.cos(Double.parseDouble(args[0])));
+        Double a = Math.sin(Double.parseDouble(args[0]));
+        Double b = Math.cos(Double.parseDouble(args[0]));
+
+        Double result = (a*a) + (b*b);
+        System.out.println(result);
+    
     }
 }
