f5f95d986817a115f7427305f9ff24e396ae1ab7
Daniel Scislowski <mr.scislowski@gmail.com>
2023-01-14 08:01:32 -0600

    added feedback

--- TrigIdentity.java
+++ TrigIdentity.java
@@ -2,10 +2,11 @@ public class TrigIdentity {
     public static void main(String[] args) {
         System.out.println(Math.sin(Double.parseDouble(args[0])));
         System.out.println(Math.cos(Double.parseDouble(args[0])));
-        Double a = Math.sin(Double.parseDouble(args[0]));
-        Double b = Math.cos(Double.parseDouble(args[0]));
+        // double != Double... you'll learn more about this later
+        double a = Math.sin(Double.parseDouble(args[0]));
+        double b = Math.cos(Double.parseDouble(args[0]));
 
-        Double result = (a*a) + (b*b);
+        double result = (a*a) + (b*b);
         System.out.println(result);
     
     }
